7f0a2130fd52ec9200c36f28e79c66b3b0baa3a0
Dougal Matthews <dougal85@gmail.com>
2013-06-13 09:40:30 +0100

    Find shortest

---
+++ team1/README.md
@@ -0,0 +1,8 @@
+Team Members
+------------
+
+Andrew Aitken
+Dougal Matthews
+Kevin McDermott
+Michael Maclean
+Tom Walker
--- team1/parse.py
+++ team1/parse.py
@@ -20,16 +20,36 @@ def check_actor_films(actor_1, actor_2):
         if actor_2 in data_set[film]:
             return film
 
+limit = None
+
 
 def main(actor_1, actor_2):
 
-    common = films_staring(actor_1).intersection(films_staring(actor_2))
-
-    if len(common) > 0:
-        return common.pop()
+    global limit
 
     films = films_staring(actor_1)
-    go_deep(films, actor_2)
+
+    best = None
+    worst = None
+
+    for i, result in enumerate(go_deep(films, actor_2)):
+
+        if best is None or len(result) < len(best):
+            best = result[:]
+            limit = len(best)
+
+        if worst is None or len(result) > len(worst):
+            worst = result
+
+    print "Common films between %s and %s" % (actor_1, actor_2)
+
+    print "Found a total of %s solutions" % i
+    print "Worst solution contained %s films" % len(worst)
+    print "Best solution contained %s films" % len(best)
+
+    print "\nLinked by the following films:"
+    for film in best:
+        print "- %s (%s)" % (film, ', '.join(sorted(data_set[film])))
 
 
 def go_deep(films, target, branch=None):
@@ -51,20 +71,14 @@ def go_deep(films, target, branch=None):
             common = check_actor_films(actor, target)
             if common:
                 branch.append(common)
-                print branch
-                print data_set[branch[0]]
-                print data_set[branch[-1]]
-                return True
-            else:
-                r = go_deep(films_staring(actor), target, branch[:])
-                if r:
-                    return True
+                yield branch
+            elif limit is None or len(branch) < limit:
+                for result in go_deep(films_staring(actor), target, branch[:]):
+                    yield result
 
 if __name__ == "__main__":
 
     actor_1 = 'YH'
     actor_2 = 'ZQ'
-
-    print actor_1, actor_2
 
     main(actor_1, actor_2)
